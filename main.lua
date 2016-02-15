61133645b36b399f1b636ee3e28ec8871afad3c8
Paulo Carabalone <psoreto@outlook.com>
2016-02-15 18:06:00 -0200

    Adicionado método para carregar as informações do anime. Removido arquivo de classes e seus construtores foram anexados ao arquivo main.lua.

--- main.lua
+++ main.lua
@@ -1,6 +1,33 @@
-require 'classes'
 local http = require 'socket.http'
 local ltn12 = require 'ltn12'
+
+--
+-- SearchItem
+--
+-- Inicia a estrutura para o objeto.
+--
+-- @param   title   Título
+-- @param   url     URL
+-- @return  table   Estrutura contendo título e url do anime.
+--
+function SearchItem(title, url)
+    return { title = title, url = url }
+end
+
+--
+-- SearchItem
+--
+-- Inicia a estrutura para o objeto.
+--
+-- @param   number  Número
+-- @param   title   Título
+-- @param   url     URL
+-- @return  table   Estrutura contendo número, título e url do episódio.
+--
+function Episode(number, title, url)
+    return { number = number, title = title, url = url }
+end
+
 
 local SuperAnimes = {}
 SuperAnimes.__index = SuperAnimes
@@ -28,7 +55,7 @@ function SuperAnimes:search(name)
     local r, c = http.request('http://www.superanimes.com/anime?&letra='..name)
     local list = {}
     local pattern = '<a href="(http://www.superanimes.com/[^/]+)" title="([^"]+)"><h2>'
-    for url, title in r:gmatch(pattern) do table.insert(list, SearchItem:new(title, url)) end
+    for url, title in r:gmatch(pattern) do table.insert(list, SearchItem(title, url)) end
     return list
 end
 
@@ -45,9 +72,9 @@ function SuperAnimes:getepisodelist(url, page)
     local r, c = http.request(url..'?&pagina='..page)
     local name = url:match('http://www.superanimes.com/([^/]+)/?')
     local list, count = {}, 1
-    for i in r:gmatch('<h3 itemprop="name">([^>]+)</h3>') do
+    for n in r:gmatch('<h3 itemprop="name">([^>]+)</h3>') do
         local cb = (page * 20) + count - 20
-        table.insert(list, Episode:new(cb, i, url..'/episodio-'..cb))
+        table.insert(list, Episode(cb, n, url..'/episodio-'..cb))
         count = count + 1
     end
     return list
@@ -86,11 +113,36 @@ function SuperAnimes:getvideourl(episodeurl)
     else return video end
 end
 
+--
+-- loadanimeinfo
+--
+-- Carrega as informações sobre anime, tais como: nome, gênero,
+-- episódios e demais.
+--
+-- @param   animeurl    URL do anime
+-- @return  table
+--
+function SuperAnimes:loadanimeinfo(searchitem)
+    local r, c = http.request(searchitem.url)
+    local info = {}
+    info.title = searchitem.title
+    info.url = searchitem.url
+    info.genre = r:match('itemprop="genre">([^>]+)</span>')
+    info.author = r:match('itemprop="author">([^>]+)</span>')
+    info.director = r:match('itemprop="director">([^>]+)</span>')
+    info.company = r:match('itemprop="productionCompany">([^>]+)</span>')
+    info.lastepisode = r:match('itemprop="numberOfEpisodes">([^>]+)</span>')
+    info.year = r:match('itemprop="copyrightYear">([^>]+)</span>')
+    return info
+end
+
 -- instancia o obeto de abstração do site
 local layer = SuperAnimes:new()
 -- pesquisa por "naruto"
 local result = layer:search('naruto')
+-- carrega as informações do primeiro item
+local anime = layer:loadanimeinfo(result[1])
 -- obtém a lista de episódios do primeiro resultado da busca
-local episodes = layer:getepisodelist(result[1].url, 3)
+local episodes = layer:getepisodelist(anime.url, 1)
 -- obtém a url direta do primeiro episódio do anime
-local videourl = layer:getvideourl(episodes[1].url)
+local videourl = layer:getvideourl(anime.url)
